489d9b360cd5b5a315522caccf6df5cf28af4989
Rachel <rstark.smith@gmail.com>
2022-10-30 13:21:21 -0500

    commit tree 3f9d1a463d9503683bfee722f53f71a198c971c2

---
+++ lib/song.rb
@@ -0,0 +1,122 @@
+require 'pry'
+
+class Song
+
+    attr_accessor :name, :artist, :genre
+   
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize (name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+
+       
+    puts Song.count
+
+    s1 = Song.new("songname", "gwen", "rap")
+    s2 = Song.new("anotherone", "jessie", "rock")
+    s3 = Song.new("dancesong", "warn", "rap")
+    s3 = Song.new("livve", "gwen", "rap")
+
+
+    puts Song.artist_count
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
+#  attr_accessor :name, :artist, :genre
+
+#     @@count = 0
+#     @@genres = []
+#     @@artists = []
+
+#     def initialize(name, artist, genre)
+#         @name = name
+#         @artist = artist
+#         @genre = genre
+#         @@count += 1
+#         @@genres << genre
+#         @@artists << artist
+#     end
+
+#     def self.count
+#         @@count
+#     end
+
+#     def self.genres
+#         @@genres.uniq
+#     end
+
+#     def self.artists
+#         @@artists.uniq
+#     end
+
+#     def self.genre_count
+#         @@genres.tally
+#     end
+
+#     #  def self.genre_count
+# #     genre_count = {}
+# #     @@genres.each do |genre|
+# #       if genre_count[genre]
+# #         genre_count[genre] += 1 
+# #       else
+# #         genre_count[genre] = 1
+# #       end
+# #     end
+# #     genre_count
+# #   end
+    
+#     def self.artist_count
+#         @@artists.tally
+#     end
